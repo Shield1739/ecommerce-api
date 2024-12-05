9802c8190c7fa5d1f457ede17e7f49c1108ee20f
Shield1739 <18101480+Shield1739@users.noreply.github.com>
2024-12-04 23:07:54 -0500

    adds cart routes tests

--- tests/routes/cart.test.js
+++ tests/routes/cart.test.js
@@ -29,32 +29,55 @@ describe('Cart Routes', () => {
   describe('POST /api/carts/:userId', () => {
     it('should create a new cart', async () => {
 
+      const userId = '10';
+      const response = await request(app)
+          .post('/api/carts/' + userId)
+          .expect(201);
+
+      expect(response.body).toHaveProperty('userId', userId);
     });
   });
 
   describe('POST /api/carts/:cartId/items', () => {
-    //let cart, product;
+    
+    const userId = '1';
+    let cart, product;
 
-    // beforeEach(async () => {
-    //   const category = await Category.create({ name: 'Test Category' });
-    //   product = await Product.create({
-    //     name: 'Test Product',
-    //     price: 100,
-    //     inventory: 10,
-    //     categoryId: category.id
-    //   });
-    //   cart = await Cart.create({ userId: '1' });
-    // });
+    beforeEach(async () => {
+      const category = await Category.create({ name: 'Test Category' });
+      product = await Product.create({
+        name: 'Test Product',
+        price: 100,
+        inventory: 10,
+        categoryId: category.id
+      });
+      cart = await Cart.create({ userId: userId });
+    });
 
     it('should add item to cart', async () => {
+      
+      const quantity = 10;
+      
+      const response = await request(app)
+          .post('/api/carts/' + cart.id + '/items')
+          .send({
+            productId: product.id,
+            quantity: quantity,
+          }).expect(201);
 
+      expect(response.body).toHaveProperty('id');
+      expect(response.body).toHaveProperty('cartId', cart.id.toString());
+      expect(response.body).toHaveProperty('productId', product.id);
+      expect(response.body).toHaveProperty('quantity', quantity);
+      expect(response.body).toHaveProperty('updatedAt');
+      expect(response.body).toHaveProperty('createdAt');
     });
 
   });
 
   describe('GET /api/carts/:cartId/items', () => {
     it('should return cart items with totals', async () => {
-
+      
     });
   });
 });
