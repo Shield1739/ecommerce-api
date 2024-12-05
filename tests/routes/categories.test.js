fd34ce707c0890e950dcbd50e0fc3a959866f460
Shield1739 <18101480+Shield1739@users.noreply.github.com>
2024-12-04 23:07:39 -0500

    adds categories routes tests

--- tests/routes/categories.test.js
+++ tests/routes/categories.test.js
@@ -1,54 +1,66 @@
 const request = require('supertest');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { initTestDb, closeTestDb } = require('../setup/testDb');
+const {initTestDb, closeTestDb} = require('../setup/testDb');
 const categoryRouter = require('../../routes/categories');
 const Category = require('../../models/category');
+// const {etag} = require("express/lib/utils");
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/api/categories', categoryRouter);
 
 describe('Category Routes', () => {
-  beforeAll(async () => {
-    await initTestDb();
-  });
-
-  afterAll(async () => {
-    await closeTestDb();
-  });
-
-  beforeEach(async () => {
-    await Category.destroy({ where: {} });
-  });
-
-  describe('POST /api/categories', () => {
-    it('should create a new category', async () => {
-      const categoryData = {
-        name: 'Electronics'
-      };
-
-      const response = await request(app)
-        .post('/api/categories')
-        .send(categoryData)
-        .expect(201);
-
-      expect(response.body).toHaveProperty('name', 'Electronics');
-      expect(response.body).toHaveProperty('id');
+    beforeAll(async () => {
+        await initTestDb();
     });
-  });
 
-  describe('GET /api/categories', () => {
-    // beforeEach(async () => {
-    //   await Category.bulkCreate([
-    //     { name: 'Electronics' },
-    //     { name: 'Books' },
-    //     { name: 'Clothing' }
-    //   ]);
-    // });
-
-    it('should return all categories', async () => {
-
+    afterAll(async () => {
+        await closeTestDb();
     });
-  });
+
+    beforeEach(async () => {
+        await Category.destroy({where: {}});
+    });
+
+    describe('POST /api/categories', () => {
+        it('should create a new category', async () => {
+            const categoryData = {
+                name: 'Electronics'
+            };
+
+            const response = await request(app)
+                .post('/api/categories')
+                .send(categoryData)
+                .expect(201);
+
+            expect(response.body).toHaveProperty('name', categoryData.name);
+            expect(response.body).toHaveProperty('id');
+        });
+    });
+
+    describe('GET /api/categories', () => {
+        let categories;
+        
+        beforeEach(async () => {
+
+            categories =
+                [
+                    {name: 'Electronics'},
+                    {name: 'Books'},
+                    {name: 'Clothing'}
+                ]
+
+            await Category.bulkCreate(categories);
+        });
+
+        it('should return all categories', async () => {
+            const response = await request(app)
+                .get('/api/categories')
+                .expect(200);
+
+            // TODO add better assert
+            expect(response.body.length).toEqual(categories.length);
+        });
+    });
 });
